f73df89ff25e9cb0795dc02db1197fbec6ec8fe2
Lucas Dias <48134977+lucaspdp@users.noreply.github.com>
2020-02-02 00:44:31 -0300

    Update deprecations

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const port  = process.env.PORT || 3000;
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: true
+  useFindAndModify: false
 }).then(()=>{
   console.log("Conectado ao BD!");
 }, ()=>{
