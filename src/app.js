da1352025b98737fc07a5de965b378582272cc6e
Lucas <lucaspdpp@gmail.com>
2022-01-06 15:19:48 -0300

    Update Find and Update

--- src/app.js
+++ src/app.js
@@ -16,7 +16,6 @@ const port  = process.env.PORT || 3000;
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: false
 }).then(()=>{
   console.log("Conectado ao BD!");
 }, (e)=>{
