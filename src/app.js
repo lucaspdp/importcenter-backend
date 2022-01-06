1acb86635b7e266126037dd3d1f4f4e3f972b24d
Lucas <lucaspdpp@gmail.com>
2022-01-06 15:12:44 -0300

    Update mongoose driver

--- src/app.js
+++ src/app.js
@@ -13,7 +13,11 @@ const app = express();
 const port  = process.env.PORT || 3000;
 
 //Inicia conexão com o BD
-mongoose.connect(process.env.MONGO_URI).then(()=>{
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+}).then(()=>{
   console.log("Conectado ao BD!");
 }, (e)=>{
   console.log("Falha ao se conectar ao BD!", e);
